3e4f8e9fc99cb853aa0ba60ad3cb8c2ac0f91a7c
Ahmad Mostafa <a.m_eg@yahoo.com>
2023-05-28 05:54:43 +0300

    Add files via upload

---
+++ youtube.py
@@ -0,0 +1,23 @@
+from pytube import YouTube
+import streamlit as st
+
+st.title("Youtube Video Downloader")
+
+def Download(link, path):
+    youtubeObject = YouTube(link)
+    youtubeObject = youtubeObject.streams.get_highest_resolution()
+    try:
+        youtubeObject.download(path)
+    except:
+        print("An error has occurred")
+    print("Download is completed successfully")
+
+
+link = st.text_input("Enter the link of the video")
+# choose the path on your computer
+path = st.text_input("Enter the path of the video")
+
+if st.button("Download"):
+    Download(link, path)
+
+# Path: youtube.py
